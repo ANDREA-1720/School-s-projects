b2dab551675417597a87fcb2ba25a80512d4ab28
ANDREA1720 <142151767+ANDREA-1720@users.noreply.github.com>
2024-11-07 17:14:08 +0100

    Added 20241125-2esa-Perciabosco-eq1gradoC

---
+++ 20241104-2esa-Perciabosco-eq1grado-negativoC/README.md
@@ -0,0 +1,3 @@
+# Autore: Andrea Perciabosco
+# Classe: 2ESA
+# Descri: "20241104-2esa-Perciabosco-eq1grado-negativoC"
---
+++ 20241104-2esa-Perciabosco-eq1grado-negativoC/main.cpp
@@ -0,0 +1,45 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 2ESA
+    Descrizione: "20241125-2esa-Perciabosco-eq1gradoC"
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    // Inizializzazione delle variabili
+    double coeffA, coeffB, coeffC;
+
+    // acquisizione input
+    cout << "Ax+B=C" << endl;
+    cout << endl;
+    cout << "Inserisci il coefficente A: ";
+    cin >> coeffA;
+    cout << "Inserisci il coefficente B: ";
+    cin >> coeffB;
+    cout << "Inserisci il coefficente C: ";
+    cin >> coeffC;
+
+    // elaborazione
+    if (coeffA == 0)
+    {
+        if (coeffC - coeffB != 0)
+        {
+            cout << "L'equazione è impossibile." << endl;
+        }
+        else
+        {
+            cout << "L'equazione è indeterminata." << endl;
+        }
+        return 0;
+    }
+
+    // comunicazione dei risultati output
+    cout << "La soluzione dell'equazione è: x = " << coeffA / (coeffC - coeffB) << "." << endl;
+
+    return 0;
+}
