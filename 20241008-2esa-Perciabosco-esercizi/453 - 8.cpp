832c653f4bee229dd12c536bfc5c14f2c7500ef3
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2024-12-02 08:25:13 +0100

    Update 453 - 8.cpp

--- 20241008-2esa-Perciabosco-esercizi/453 - 8.cpp
+++ 20241008-2esa-Perciabosco-esercizi/453 - 8.cpp
@@ -1,7 +1,7 @@
 /*
     Autore: Andrea Perciabosco
     Classe: 2ESA
-    Descrizione: "Date le lunghezze dei lati di un triangolo, stabilisce se il triangolo è equilatero, isoscele o scaleno."
+    Descrizione: "20241125-2esa-Perciabosco-tipo-triangoloC"
 */
 
 #include <iostream>
@@ -17,12 +17,19 @@ int main()
     cin >> lat2;
     cout << "Inserisci la lunghezza del terzo lato: ";
     cin >> lat3;
-
-    if (lat1 == lat2 && lat2 == lat3)
-        cout << "Il triangolo è equilatero." << endl;
-    else if (lat1 == lat2 || lat2 == lat3 || lat3 == lat1)
-        cout << "Il triangolo è isoscele." << endl;
-    else
-        cout << "Il triangolo è scaleno." << endl;
+	lat1 = abs(lat1);
+	lat2 = abs(lat2);
+	lat3 = abs(lat3)
+	if(lat1 == 0 || lat2 == 0 || lat3 == 0){
+		cout << "E' stato inserito un numero uguale a 0" <<endl;
+	}else if((lat1 > abs(lat2-lat3) && lat1<lat2+lat3) && (lat2 > abs(lat1-lat3) && lat2<lat1+lat3) && (lat3 > abs(lat2-lat1) && lat3<lat2+lat1))
+	{
+		if (lat1 == lat2 && lat2 == lat3)
+        	cout << "Il triangolo è equilatero." << endl;
+    	else if (lat1 == lat2 || lat2 == lat3 || lat3 == lat1)
+        	cout << "Il triangolo è isoscele." << endl;
+    	else
+        	cout << "Il triangolo è scaleno." << endl;
+	}
     return 0;
 }
