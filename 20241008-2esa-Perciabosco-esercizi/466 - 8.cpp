bb6b7286b98ca6db930d1580afea1b9efffd5555
ANDREA1720 <142151767+ANDREA-1720@users.noreply.github.com>
2024-12-15 17:12:50 +0100

    Updated 20241210-2esa-Perciabosco-impostaC

--- 20241008-2esa-Perciabosco-esercizi/466 - 8.cpp
+++ 20241008-2esa-Perciabosco-esercizi/466 - 8.cpp
@@ -15,46 +15,44 @@ int main()
     cout << "Inserisci il valore di cui calcolare l'imposta: ";
     cin >> num;
 
-    if (num > 0)
+    if (num <= 5000)
     {
-        if (num <= 5000)
+        imposta = num * 0.05;
+    }
+    else
+    {
+        imposta += 5000 * 0.05;
+
+        if (num <= 15000)
         {
-            imposta = num * 0.05;
+            imposta += (num - 5000) * 0.08;
         }
         else
         {
-            imposta += 5000 * 0.05;
+            imposta += (15000 - 5000) * 0.08;
 
-            if (num <= 15000)
+            if (num <= 25000)
             {
-                imposta += (num - 5000) * 0.08;
+                imposta += (num - 15000) * 0.18;
             }
             else
             {
-                imposta += (15000 - 5000) * 0.08;
+                imposta += (25000 - 15000) * 0.18;
 
-                if (num <= 25000)
+                if (num <= 35000)
                 {
-                    imposta += (num - 15000) * 0.18;
+                    imposta += (num - 25000) * 0.24;
                 }
                 else
                 {
-                    imposta += (25000 - 15000) * 0.18;
+                    imposta += (35000 - 25000) * 0.24;
 
-                    if (num <= 35000)
-                    {
-                        imposta += (num - 25000) * 0.24;
-                    }
-                    else
-                    {
-                        imposta += (35000 - 25000) * 0.24;
-
-                        imposta += (num - 35000) * 0.36;
-                    }
+                    imposta += (num - 35000) * 0.36;
                 }
             }
         }
     }
+
     cout << "L'imposta equivale a " << imposta << "€, quindi il totale è di " << num + imposta << "€." << endl;
     return 0;
 }
