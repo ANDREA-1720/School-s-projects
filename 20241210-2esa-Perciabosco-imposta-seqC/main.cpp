19f951ba9842cd655e512c37a8fd065201953d85
ANDREA1720 <142151767+ANDREA-1720@users.noreply.github.com>
2024-12-15 18:14:41 +0100

    Fixed the implementation of 0241210-2esa-Perciabosco-imposta-seqC

--- 20241210-2esa-Perciabosco-imposta-seqC/main.cpp
+++ 20241210-2esa-Perciabosco-imposta-seqC/main.cpp
@@ -5,37 +5,38 @@
 */
 
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
     // Inizializzazione delle variabili
-    double numero, imposta = 0.0;
+    double numero, imposta = 0.0, eccedenza;
 
-    // acquisizione input
+    // Acquisizione input
     cout << "Inserisci il valore di cui calcolare l'imposta: ";
     cin >> numero;
 
-    // elaborazione dei dati
-
     // Scaglione 1: 0-5000 al 5%
-    imposta += ((5000 + min(numero, 5000.0) - abs(5000 - min(numero, 5000.0))) / 2) * 0.05;
+    imposta += ((numero + 5000 - abs(numero - 5000)) / 2) * 0.05;
 
     // Scaglione 2: 5000-15000 al 8%
-    imposta += ((15000 + min(max(numero - 5000.0, 0.0), 10000.0) - abs(15000 - min(max(numero - 5000.0, 0.0), 10000.0))) / 2 + abs((15000 + min(max(numero - 5000.0, 0.0), 10000.0) - abs(15000 - min(max(numero - 5000.0, 0.0), 10000.0))) / 2)) / 2 * 0.08;
+    eccedenza = ((numero - 5000) + abs(numero - 5000)) / 2;
+    imposta += ((eccedenza + 10000 - abs(eccedenza - 10000)) / 2) * 0.08;
 
     // Scaglione 3: 15000-25000 al 18%
-    imposta += ((25000 + min(max(numero - 15000.0, 0.0), 10000.0) - abs(25000 - min(max(numero - 15000.0, 0.0), 10000.0))) / 2 + abs((25000 + min(max(numero - 15000.0, 0.0), 10000.0) - abs(25000 - min(max(numero - 15000.0, 0.0), 10000.0))) / 2)) / 2 * 0.18;
+    eccedenza = ((numero - 15000) + abs(numero - 15000)) / 2;
+    imposta += ((eccedenza + 10000 - abs(eccedenza - 10000)) / 2) * 0.18;
 
     // Scaglione 4: 25000-35000 al 24%
-    imposta += ((35000 + min(max(numero - 25000.0, 0.0), 10000.0) - abs(35000 - min(max(numero - 25000.0, 0.0), 10000.0))) / 2 + abs((35000 + min(max(numero - 25000.0, 0.0), 10000.0) - abs(35000 - min(max(numero - 25000.0, 0.0), 10000.0))) / 2)) / 2 * 0.24;
+    eccedenza = ((numero - 25000) + abs(numero - 25000)) / 2;
+    imposta += ((eccedenza + 10000 - abs(eccedenza - 10000)) / 2) * 0.24;
 
-    // Resto al 36%
+    // Oltre 35000 al 36%
     imposta += ((numero - 35000) + abs(numero - 35000)) / 2 * 0.36;
 
-    // comunicazione dei risultati output
+    // Comunicazione dei risultati
     cout << "L'imposta equivale a " << imposta << "€, quindi il totale è di " << numero + imposta << "€." << endl;
     return 0;
 }
