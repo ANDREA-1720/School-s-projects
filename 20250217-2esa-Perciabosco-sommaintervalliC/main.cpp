4c404b21cdb7aa345d12c2377177dc716a6faa22
ANDREA1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-02-17 17:55:56 +0100

    Added 20250217-2esa-Perciabosco-sommaintervalliC

---
+++ 20250217-2esa-Perciabosco-sommaintervalliC/main.cpp
@@ -0,0 +1,25 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 2ESA
+    Descrizione: "20250217-2esa-Perciabosco-sommaintervalliC"
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int num, somma = 0, indice = 0;
+    cout << "Inserisci un numero: ";
+    cin >> num;
+    num = abs(num);
+    while (indice < num)
+    {
+        indice++;
+        somma += indice;
+    }
+    cout << "La somma di tutti i numeri compresi tra 1 e " << num << " e': " << somma << endl;
+    return 0;
+}
