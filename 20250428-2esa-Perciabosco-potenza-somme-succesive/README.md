541d548a57aa2003d7f3e6feff73d874da1c2e72
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-05-05 09:41:14 +0000

    Added 20250428-2esa-Perciabosco-potenza-somme-succesive

---
+++ 20250428-2esa-Perciabosco-potenza-somme-succesive/README.md
@@ -0,0 +1,3 @@
+# Autore: Andrea Perciabosco
+# Classe: 2ESA
+# Descri: "220250428-2esa-Perciabosco-potenza-somme-succesive"
---
+++ 20250428-2esa-Perciabosco-potenza-somme-succesive/main.cpp
@@ -0,0 +1,58 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 2esa
+    Descri: 202504428-2esa-Perciabosco-potenza-somme-succesive
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int PERpotenza = 1, PERbase, PEResponente, PERconta, PERindice, PERsegno, PERtemp;
+    cout << "Inserisci la base della potenza" << endl;
+    cin >> PERbase;
+    cout << "Inserisci l'esponente" << endl;
+    cin >> PEResponente;
+    PEResponente = abs(PEResponente);
+
+    if (PEResponente == 0)
+    {
+        PERpotenza = 1;
+    }
+    else
+    {
+        if (PERbase == 0)
+        {
+            PERpotenza = 0;
+        }
+        else
+        {
+            if (PEResponente % 2 == 0)
+            {
+                PERsegno = 1;
+            }
+            else
+            {
+                PERsegno = PERbase / abs(PERbase);
+            }
+
+            PERpotenza = 1;
+            for (PERconta = 0; PERconta < PEResponente; PERconta++)
+            {
+                PERtemp = 0;
+                for (PERindice = 0; PERindice < abs(PERbase); PERindice++)
+                {
+                    PERtemp += PERpotenza;
+                }
+                PERpotenza = PERtemp;
+            }
+
+            PERpotenza *= PERsegno;
+        }
+    }
+
+    cout << PERbase << " elevato alla " << PEResponente << " è uguale a " << PERpotenza << "." << endl;
+    return 0;
+}
