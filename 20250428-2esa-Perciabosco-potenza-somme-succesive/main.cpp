2b343238a31ab55601936efa844e9d63400c8185
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-05-12 12:17:08 +0200

    Code clean up

--- 20250428-2esa-Perciabosco-potenza-somme-succesive/main.cpp
+++ 20250428-2esa-Perciabosco-potenza-somme-succesive/main.cpp
@@ -15,43 +15,35 @@ int main()
     cin >> base;
     cout << "Inserisci l'esponente" << endl;
     cin >> esponente;
-    esponente = abs(esponente);
 
-    if (base == 0)
+    if (esponente % 2 == 0)
     {
-        potenza = 0;
+        segno = 1;
     }
     else
     {
-        if (esponente % 2 == 0)
-        {
-            segno = 1;
-        }
-        else
-        {
-            segno = base / abs(base);
-        }
-
-        potenza = 1;
-        for (conta = 0; conta < esponente; conta++)
-        {
-            temp = 0;
-            for (indice = 0; indice < abs(base); indice++)
-            {
-                temp += potenza;
-            }
-            potenza = temp;
-        }
-
-        potenza *= segno;
+        segno = base / abs(base);
     }
 
+    potenza = 1;
+    for (conta = 0; conta < abs(esponente); conta++)
+    {
+        temp = 0;
+        for (indice = 0; indice < abs(base); indice++)
+        {
+            temp += potenza;
+        }
+        potenza = temp;
+    }
+
+    potenza *= segno;
+    
+    cout << base << " elevato alla " << esponente << " è uguale a ";
     if(esponente < 0)
     {
-        cout << base << " elevato alla " << esponente << " è uguale a 1/" << potenza << "." << endl;
-    } else
-    {
-        cout << base << " elevato alla " << esponente << " è uguale a " << potenza << "." << endl;
+        cout << "1/";
     }
+    cout << potenza << "." << endl;
+    
     return 0;
 }
