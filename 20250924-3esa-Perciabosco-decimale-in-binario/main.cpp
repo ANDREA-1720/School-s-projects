ed19abb3fe7cc20345e9c9074d3d7c3ab0370fc5
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-10-02 10:16:29 +0200

    Improved negative number handling in 20250924-3esa-Perciabosco-decimale-in-binario

--- 20250924-3esa-Perciabosco-decimale-in-binario/main.cpp
+++ 20250924-3esa-Perciabosco-decimale-in-binario/main.cpp
@@ -12,16 +12,18 @@ using namespace std;
 int main() {
   long long decimale;
   unsigned long long binario = 0;
-  int indice, cifra;
+  int indice, cifra, segno;
   cout << "Inserisci un numero decimale" << endl;
   cin >> decimale;
 
+  segno = decoimale/abs(decimale);
   decimale = abs(decimale);    
   for (indice = 0; decimale > 0; indice++) {
     cifra = decimale % 2;
     binario += cifra * pow(10, indice);
     decimale = (decimale - cifra) / 2;
   }
+  decimale *= segno;
   
   cout << "Il numero decimale " << decimale << " è rappresentato in binario con " << binario << "." << endl;
   return 0;
