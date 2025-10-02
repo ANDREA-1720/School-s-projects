a6ca48a9a249f1e6282de2a8296d3905fa66d993
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-10-02 10:04:43 +0200

    Update 20250924-3esa-Perciabosco-decimale-in-binario

--- 20250924-3esa-Perciabosco-decimale-in-binario/main.cpp
+++ 20250924-3esa-Perciabosco-decimale-in-binario/main.cpp
@@ -10,7 +10,8 @@
 using namespace std;
 
 int main() {
-  long long decimale, binario = 0;
+  long long decimale;
+  unsigned long long binario = 0;
   int indice, cifra;
   cout << "Inserisci un numero decimale" << endl;
   cin >> decimale;
