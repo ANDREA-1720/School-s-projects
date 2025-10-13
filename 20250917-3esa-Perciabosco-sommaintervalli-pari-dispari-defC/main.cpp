7e2b83e1a87b2e64eab8b6abc5acf729d247a385
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-10-13 06:57:19 +0000

    Added sommaintervalli-pari-dispari

---
+++ 20250917-3esa-Perciabosco-sommaintervalli-pari-dispari-defC/main.cpp
@@ -0,0 +1,30 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 3ESA
+    Descrizione: "20250917-2esa-Perciabosco-sommaintervalli-pari-dispari-defC"
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int num, sommaPari = 0, sommaDispari = 0, indice = 0;
+    cout << "Inserisci un numero: ";
+    cin >> num;
+    num = abs(num);
+
+    for (indice = 1; indice <= num; indice++)
+    {
+        if(indice % 2 == 0){
+            sommaPari += indice;
+        } else {
+            sommaDispari += indice;
+        }
+    }
+
+    cout << "La somma di tutti i numeri pari  compresi tra 1 e " << num << " e': " << sommaPari << ", mentre quella dei numeri dispari è " << sommaDispari << "." << endl;
+    return 0;
+}
---
+++ 20250917-3esa-Perciabosco-sommaintervalli-pari-dispari-preC/main.cpp
@@ -0,0 +1,29 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 3ESA
+    Descrizione: "20250917-2esa-Perciabosco-sommaintervalli-pari-dispari-preC"
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int num, somma = 0, indice = 0;
+    cout << "Inserisci un numero: ";
+    cin >> num;
+    num = abs(num);
+    while (indice < num)
+    {
+        indice++;
+        if(indice % 2 == 0){
+            sommaPari += indice;
+        } else {
+            sommaDispari += indice;
+        }
+    }
+    cout << "La somma di tutti i numeri pari  compresi tra 1 e " << num << " e': " << sommaPari << ", mentre quella dei numeri dispari è " << sommaDispari << "." << endl;
+    return 0;
+}
