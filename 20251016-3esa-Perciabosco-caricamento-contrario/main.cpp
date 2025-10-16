0e9c5ed6f855d1a1951984ca5d43b2b99d0229db
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-10-16 07:58:54 +0000

    Added 20251016-3esa-Perciabosco-caricamento-contrario

---
+++ 20251016-3esa-Perciabosco-caricamento-contrario/main.cpp
@@ -0,0 +1,50 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 2ESA
+    Descrizione:
+   "20251016-3esa-Perciabosco-caricamento-contrario"
+*/
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  srand((unsigned)time(NULL));
+  int indice/*=0*/, indice2 ,comodo, dati, min, max, cifra;
+  cout << "Inserisci la quantità di dati da generare" << endl;
+  cin >> dati;
+  cout << "Inserisci il valore minimo dei dati da generare" << endl;
+  cin >> min;
+  cout << "Inserisci il valore massimo dei dati da generare" << endl;
+  cin >> max;
+  int vetnum1[dati];
+  int vetcont[dati];
+
+  if(max<min){
+      min = min + max;
+      max = min - max;
+      min = min - max;
+  }
+  cout << min << endl;
+  cout << max << endl;
+  for (indice = 0; indice < dati; indice++) { //while(indice<dati){
+    comodo = rand() % (max - min + 1) + min;
+    vetnum1[indice] = comodo;
+    vetcont[indice] = 0;
+    // indice2 = 0;
+    for(indice2 = 0; comodo != 0; indice2++){ //while(comodo != 0){
+      cifra = comodo % 10;
+      vetcont[indice] = vetcont[indice] * 10 + cifra;
+      comodo /= 10;
+      //indice2++
+    }
+    cout << "Il valore in posizione " << indice << " è " << vetnum1[indice] << " che al contrario è scritto " << vetcont[indice] << "." << endl;
+    //indice++;
+  }
+
+  return 0;
+}
