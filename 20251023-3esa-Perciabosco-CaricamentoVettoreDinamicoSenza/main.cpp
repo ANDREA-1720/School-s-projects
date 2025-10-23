f7a467106fd92d9a9579389143b5a2bb1fc26d10
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-10-23 10:12:37 +0200

    Fix duplicate check logic in comment for dynamic vector loading

--- 20251023-3esa-Perciabosco-CaricamentoVettoreDinamicoSenza/main.cpp
+++ 20251023-3esa-Perciabosco-CaricamentoVettoreDinamicoSenza/main.cpp
@@ -38,7 +38,7 @@ int main() {
     duplicato = 0;
     comodo = rand() % (max - min + 1) + min;
     // indice2 = 0
-    for(indice2 = 0; indice2 < indice; indice2++){ // while(indice2 < indice && duplicato == 0){
+    for(indice2 = 0; indice2 < indice; indice2++){ // while(indice2 < indice){
       if(vetnum1[indice] == vetnum1[indice2]){
         duplicato = 1;
         indice2 = indice;
