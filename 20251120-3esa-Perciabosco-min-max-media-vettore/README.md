2186923d31cc367570c73c671ae09ce382225785
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2025-11-24 07:43:01 +0000

    Added 20251120-3esa-Perciabosco-min-max-media-vettore

---
+++ 20251120-3esa-Perciabosco-min-max-media-vettore/README.md
@@ -0,0 +1,3 @@
+# Autore: Andrea Perciabosco
+# Classe: 3ESA
+# Descri:"20251120-3esa-Perciabosco-min-max-media-vettore"
---
+++ 20251120-3esa-Perciabosco-min-max-media-vettore/main.cpp
@@ -0,0 +1,85 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 3ESA
+    Descrizione: "20251120-3esa-Perciabosco-min-max-media-vettore"
+*/
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  srand((unsigned)time(NULL));
+  int indice/*=0*/, indice2 ,comodo, dati, min, max, cifra, duplicato, posMin, posMax, posMedia, diff, somma=0;
+  double media;
+  cout << "Inserisci la quantità di dati da generare" << endl;
+  cin >> dati;
+  do{
+    cout << "Inserisci il valore minimo dei dati da generare" << endl;
+    cin >> min;
+    cout << "Inserisci il valore massimo dei dati da generare" << endl;
+    cin >> max;
+    if(abs(max-min) < dati){
+      cout<<"Intervallo non valido, non è abbastanza grade per riempire il vettore con soli numeri distinti." << endl;
+    }
+  } while(abs(max-min) < dati);
+  int vetnum[dati];
+
+  dati=abs(dati);
+  if(max<min){
+    min = min + max;
+    max = min - max;
+    min = min - max;
+  }
+
+  posMin = 0;
+  posMax = dati - 1;
+
+  for (indice = 0; indice < dati; indice++) { // while(indice < dati){
+    duplicato = 0;
+    comodo = rand() % (max - min + 1) + min;
+    // indice2 = 0
+    for(indice2 = 0; indice2 < indice; indice2++){ // while(indice2 < indice){
+      if(vetnum[indice] == vetnum[indice2]){
+        duplicato = 1;
+        indice2 = indice;
+      }
+      // indice2++;
+    }
+    
+    if(duplicato != 0){
+      indice--;
+    } else {
+      vetnum[indice] = comodo;
+      somma += vetnum[indice];
+      cout << "Il valore in posizione " << indice << " è " << vetnum[indice] << "." << endl;
+
+      if(comodo < vetnum[posMin]){
+        posMin = indice;
+      } else if(comodo > vetnum[posMax]){
+        posMax = indice;
+      }
+    }
+    // indice++;
+  }
+
+  media = (double) somma / dati;
+  diff = max - min + 1;
+  for(indice = 0; indice < dati; indice++){
+    comodo = abs(vetnum[indice] - round(media));
+    if(comodo < diff){
+      diff = comodo;
+      posMedia = indice;
+    }
+  }
+
+  cout << endl;
+  cout << "Il valore minimo è " << vetnum[posMin] << " in posizione " << posMin << "." << endl;
+  cout << "Il valore massimo è " << vetnum[posMax] << " in posizione " << posMax << "." << endl;
+  cout << "La media è " << media << " e il valore più vicino è " << vetnum[posMedia] << " in posizione " << posMedia << "." << endl;
+
+  return 0;
+}
