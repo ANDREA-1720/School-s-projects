02906ce8e4a94c49880d31483c8f4d590fb42366
ANDREA-1720 <142151767+ANDREA-1720@users.noreply.github.com>
2026-05-14 10:25:59 +0000

    20260504-3esa-Perciabosco-Numeri-Particolari-function

---
+++ 20260504-3esa-Perciabosco-Numeri-Particolari-function/README.md
@@ -0,0 +1,3 @@
+# Autore: Andrea Perciabosco
+# Classe: 2ESA
+# Descri: "20260504-3esa-Perciabosco-Numeri-Particolari-function"
---
+++ 20260504-3esa-Perciabosco-Numeri-Particolari-function/main.cpp
@@ -0,0 +1,41 @@
+/*
+    Autore: Andrea Perciabosco
+    Classe: 2ESA
+    Descrizione: "20260504-3esa-Perciabosco-Numeri-Particolari-function"
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int fncSommaDivisori(int num, int i, int somma){
+    if(i > num / 2){
+        return somma;
+    }
+
+    if(num % i == 0){
+        somma += i;
+    }
+
+    return fncSommaDivisori(num, i + 1, somma);
+}
+
+int main()
+{
+    int num, somma;
+    cout << "Inserisci un numero: ";
+    cin >> num;
+
+    somma = fncSommaDivisori(abs(num), 1, 0);
+
+    if(somma > num){
+        cout << "Il numero " << num << " è abbondante." << endl;
+    } else if(somma < num){
+        cout << "Il numero " << num << " è difettivo." << endl;
+    } else{
+        cout << "Il numero " << num << " è perfetto." << endl;
+    }
+
+    return 0;
+}
